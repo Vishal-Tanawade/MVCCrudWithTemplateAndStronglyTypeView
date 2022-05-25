c102891e2388be59f7604cc66901f517f144cbef
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-25 22:57:19 +0530

    Added controller with read/write action

---
+++ MVCCrudWithTemplateAndStronglyTypeView/Controllers/EmployeeManagementPortalController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCrudWithTemplateAndStronglyTypeView.Controllers
+{
+    public class EmployeeManagementPortalController : Controller
+    {
+        // GET: EmployeeManagementPortal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: EmployeeManagementPortal/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeeManagementPortal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeManagementPortal/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeManagementPortal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeManagementPortal/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeManagementPortal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeManagementPortal/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
